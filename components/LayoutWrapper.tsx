72e054ab9e2102b66a8bb7abd851e383ff81d4c6
Azaeres <azaeres@gmail.com>
2023-07-26 03:18:46 -0700

    pt

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -4,8 +4,8 @@ import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
-// import MobileNav from './MobileNav'
-// import ThemeSwitch from './ThemeSwitch'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
 import { ReactNode } from 'react'
 
 interface Props {
@@ -33,9 +33,9 @@ const LayoutWrapper = ({ children }: Props) => {
               </div>
             </Link>
           </div>
-          {/* <div className="flex items-center text-base leading-5">
+          <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
+              {/* {headerNavLinks.map((link) => (
                 <Link
                   key={link.title}
                   href={link.href}
@@ -43,11 +43,11 @@ const LayoutWrapper = ({ children }: Props) => {
                 >
                   {link.title}
                 </Link>
-              ))}
+              ))} */}
             </div>
             <ThemeSwitch />
             <MobileNav />
-          </div> */}
+          </div>
         </header>
         <main className="mb-auto">{children}</main>
         <Footer />
